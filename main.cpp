b28b59b00f6bf2dcb945e5ff29ee1a893d3672f3
jvmcpheron <janemcpheron@gmail.com>
2023-10-17 19:16:31 -0500

    solved with tests

--- main.cpp
+++ main.cpp
@@ -4,12 +4,23 @@
 
 /*
 CODE RUNNING EXAMPLES
+                                                     
+jvmcpheron@dyn-10-140-246-187 CS2_HW7 % ./main
+Size: 0
+Size: 1
+Size: 2
+Size: 3
+Size: 4
+Front element: This
+Pop This
+Size: 3
+Front element after pop: Assignment
+Pop Assignment
+Size: 2
+Front element after pop: Is
+Size: 2
+ Is Difficult !!!!%                                                                     
 
-./main
-Front element: Apple
-Front element after pop: Banana
-Front element after more pushes: Banana
-jvmcpheron@dyn-10-140-246-187 CS2_HW7 % 
 */
 
 #include <iostream>
@@ -30,7 +41,9 @@ public:
     bool empty() const {
         return size == 0;
     }
-
+    int getSize(){
+        return size;
+    }
     void push(const std::string &value) {
         if (size == capacity) {
 
@@ -64,10 +77,11 @@ public:
         size++;
     }
 
-    void pop() {
+    std::string pop() {
         if (!empty()) {
+            std::string poppedValue = data[front];
             if (front == rear) {
-
+                
 
                 // If only one element is left, reset the queue
                 front = -1;
@@ -76,6 +90,7 @@ public:
                 front = (front + 1) % capacity;
             }
             size--;
+            return poppedValue;
         }else{
             std::cerr << "You're trynna pop an empty queue buddy. Get help.";
         }
@@ -89,36 +104,66 @@ public:
     }
 };
 
+bool unitTest(){
+        CircularQueue queue = CircularQueue(2);
 
+    // Push some elements into the queue
+
+    if (!queue.empty()){
+        std::cerr << "Queue empty error";
+    }
+
+    std::cout << "Size: " << queue.getSize() << std::endl;
+    queue.push("This");
+    std::cout << "Size: " << queue.getSize() << std::endl;
+    queue.push("Assignment");
+    std::cout << "Size: " << queue.getSize() << std::endl;
+    queue.push("Is");
+    std::cout << "Size: " << queue.getSize() << std::endl;
+    queue.push("Difficult");
+    std::cout << "Size: " << queue.getSize() << std::endl;
+
+    if (queue.empty()){
+        std::cerr << "Queue full error";
+    }
+    // Display the front element
+    std::cout << "Front element: " << queue.frontValue() << std::endl;
+
+    // Pop an element
+    std::string temp = queue.pop();
+    if (temp != "This"){std::cerr <<"Wrong Value error";}
+    std::cout << "Pop " << temp << std::endl;
+    std::cout << "Size: " << queue.getSize() << std::endl;
+
+    // Display the front element after popping
+    std::cout << "Front element after pop: " << queue.frontValue() << std::endl;
+
+    // Pop an element
+    std::cout << "Pop " << queue.pop() << std::endl;
+    std::cout << "Size: " << queue.getSize() << std::endl;
+
+    // Display the front element after popping
+    std::cout << "Front element after pop: " << queue.frontValue() << std::endl;    
+
+    std::cout << "Size: " << queue.getSize() << std::endl;
+
+    queue.push("!!!!");
+
+
+
+    while(queue.getSize() != 0){
+        std::cout << " " << queue.pop();
+    }
+
+
+    // Display the front element after more pushes
+    return 0;
+}
 
 
 
 
 int main() {
-    CircularQueue queue;
+    unitTest();    
 
-    // Push some elements into the queue
-    queue.push("This");
-    queue.push("Assignment");
-    queue.push("Is");
-    queue.push("Difficult");
-
-    // Display the front element
-    std::cout << "Front element: " << queue.frontValue() << std::endl;
-
-    // Pop an element
-    queue.pop();
-
-    // Display the front element after popping
-    std::cout << "Front element after pop: " << queue.frontValue() << std::endl;
-
-    // Push more elements to demonstrate the circular behavior
-    queue.push("Elderberry");
-    queue.push("Fig");
-
-
-    // Display the front element after more pushes
-    std::cout << "Front element after more pushes: " << queue.frontValue() << std::endl;
-
-    return 0;
 }
